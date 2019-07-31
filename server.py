2f09c71a3d1613d9ac102299ecc3eb2a71b8494f
Oradenes <oradenes@gmail.com>
2019-07-31 09:40:02 +0200

    index.html done

--- server.py
+++ server.py
@@ -1,17 +1,24 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
+import data_manager
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', scores=data_manager.getScores())
 
 
 @app.route('/game')
 def game():
     return render_template('game.html')
 
+
+@app.route('/addScore', methods=['POST'])
+def addScore():
+    data_manager.addScore(request.form['name'], request.form['score'])
+
+    return redirect('/')
 
 
 if __name__ == '__main__':

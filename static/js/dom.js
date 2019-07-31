365cf2c85b57f1cf930d13d4780079fefe6f1485
Gyula Hódosi <gyula.g.hodosi@gmail.com>
2019-07-31 09:34:30 +0200

    Implemented snake movement

--- static/js/dom.js
+++ static/js/dom.js
@@ -1,3 +1,36 @@
+// global variables for session
+let snakeAdd = {x: 17, y: 9};
+let snakeDel = {x: 16, y: 8};
+var moveRight;
+var moveLeft;
+var moveUp;
+var moveDown;
+
+function snakeMoveRight() {
+    snakeAdd.x ++;
+    getCoordinates(snakeAdd, snakeDel);
+}
+
+function snakeMoveLeft() {
+    snakeAdd.x --;
+    getCoordinates(snakeAdd, snakeDel);
+}
+
+function snakeMoveUp() {
+    snakeAdd.y --;
+    getCoordinates(snakeAdd, snakeDel);
+}
+
+function snakeMoveDown() {
+    snakeAdd.y ++;
+    getCoordinates(snakeAdd, snakeDel);
+}
+
+function deleteSnake() {
+    snakeDel.x = snakeAdd.x;
+    snakeDel.y = snakeAdd.y;
+}
+
 function checkHitApple(gameCell) {
     if (gameCell.classList.contains("snake") && gameCell.classList.contains("apple")) {
         gameCell.classList.remove("apple");
@@ -22,14 +55,13 @@ function placeApple() {
 
 
 function controls() {
-    let snakeAdd = {x: 17, y: 9};
-    let snakeDel = {x: 16, y: 8};
 
     var arrowKeys = {
         left: 37,
         up: 38,
         right: 39,
-        down: 40
+        down: 40,
+        space: 32
     };
 
     document.onkeydown = function () {
@@ -37,44 +69,80 @@ function controls() {
         switch (window.event.keyCode) {
 
             case arrowKeys.left:
+                clearInterval(moveRight);
+                clearInterval(moveLeft);
+                clearInterval(moveDown);
+                clearInterval(moveUp);
                 snakeDel.x = snakeAdd.x;
                 snakeDel.y = snakeAdd.y;
-                snakeAdd.x -= 1;
-                getCoordinates(snakeAdd, snakeDel);
+                moveLeft = setInterval(snakeMoveLeft, 100);
+                setInterval(deleteSnake, 100);
+
                 break;
             case arrowKeys.up:
+                clearInterval(moveRight);
+                clearInterval(moveLeft);
+                clearInterval(moveDown);
+                clearInterval(moveUp);
                 snakeDel.x = snakeAdd.x;
                 snakeDel.y = snakeAdd.y;
-                snakeAdd.y -= 1;
-                getCoordinates(snakeAdd, snakeDel);
+                moveUp = setInterval(snakeMoveUp, 100);
+                setInterval(deleteSnake, 100);
+
                 break;
             case arrowKeys.right:
+                clearInterval(moveRight);
+                clearInterval(moveLeft);
+                clearInterval(moveDown);
+                clearInterval(moveUp);
                 snakeDel.x = snakeAdd.x;
                 snakeDel.y = snakeAdd.y;
-                snakeAdd.x += 1;
-                getCoordinates(snakeAdd, snakeDel);
+                moveRight = setInterval(snakeMoveRight, 100);
+                setInterval(deleteSnake, 100);
+
                 break;
             case arrowKeys.down:
+                clearInterval(moveRight);
+                clearInterval(moveLeft);
+                clearInterval(moveDown);
+                clearInterval(moveUp);
                 snakeDel.x = snakeAdd.x;
                 snakeDel.y = snakeAdd.y;
-                snakeAdd.y += 1;
-                getCoordinates(snakeAdd, snakeDel);
+                moveDown = setInterval(snakeMoveDown, 100);
+                setInterval(deleteSnake, 100);
+
                 break;
+            case arrowKeys.space:
+                clearInterval(moveRight)
+                clearInterval(moveLeft);
+                clearInterval(moveDown);
+                clearInterval(moveUp);
         }
     };
-    return [snakeAdd, snakeDel]
+
 }
 
 
 
-function getCoordinates(snakeAdd, snakeDel) {
+function getCoordinates() {
     let gameBoard = document.querySelectorAll('.board-cell');
 
     for (let gameCell of gameBoard) {
         let cordY = parseInt(gameCell.dataset.coordinateY);
         let cordX = parseInt(gameCell.dataset.coordinateX);
-        let cordCell = {x: cordX, y: cordY}
-        gameCell.textContent = cordY + ',' + cordX;
+        //gameCell.textContent = cordY + ',' + cordX;
+        if (snakeAdd.x > 35) {
+            snakeAdd.x = 0;
+        }
+        if (snakeAdd.x < 0) {
+            snakeAdd.x = 35;
+        }
+        if (snakeAdd.y > 18) {
+            snakeAdd.y = 0;
+        }
+        if (snakeAdd.y < 0) {
+            snakeAdd.y = 18;
+        }
         if (cordX === snakeAdd.x && cordY === snakeAdd.y) {
             gameCell.classList.add("snake")
         }
@@ -86,14 +154,14 @@ function getCoordinates(snakeAdd, snakeDel) {
 }
 
 
+
 function game() {
-    let cordApple = placeApple();
-    let snakeAdd = controls()[0];
+    placeApple();
+    //let snakeAdd = controls()[0];
     let snakeDel = controls()[1];
-    getCoordinates(snakeAdd, snakeDel);
-
-
+    getCoordinates();
 }
+
 
 
 game();
